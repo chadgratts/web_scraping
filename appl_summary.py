d8f5f213328b3da0cc5ac1807d39c252b3d51bb2
chadgratts <89122325+chadgratts@users.noreply.github.com>
2025-09-09 16:32:01 -0400

    summarize text from elements

--- appl_summary.py
+++ appl_summary.py
@@ -17,5 +17,25 @@ trs = soup.find_all("ul", class_="yf-13serlv")
 # print(trs[0].contents[1])
 # trs[i].contents[j] (every li)
 
+names = []
+values = []
 
+namVal = {}
+
+for i in range(len(trs)): # ul is 0
+    for j in range(len(trs[i].contents)): # list of mostly li, some are ' '
+        element = trs[i].contents[j]
+        # Check if it's an actual HTML element (not just whitespace text)
+        if hasattr(element, 'contents'): # its a li
+            try:
+                name = element.contents[0].text
+                names.append(name)
+            except:
+                continue
+            try:
+                value = element.contents[2].text
+                values.append(value)
+            except:
+                pass
+    namVal = dict(zip(names, values))
 
